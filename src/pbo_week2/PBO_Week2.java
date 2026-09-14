5973238aafabc81a58c486a083ce122fe99e0e6d
Grace Charity Simatupang <simatupanggrace8@gmail.com>
2026-09-14 16:38:50 +0700

    Latihan

---
+++ src/pbo_week2/PBO_Week2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo_week2;
+
+/**
+ *
+ * @author Grace
+ */
+public class PBO_Week2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
